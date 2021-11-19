afdb259bb27bebed978ff856639fc3c3e6fd5a9c
khanhhhungg <94662893+khanhhhungg@users.noreply.github.com>
2021-11-19 10:23:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    introTitle: 'Nguyễn Mai Thu Uyên <3 ',
+    introDesc: `Em hong biết sao nhưng mà em 
+    phải cảm ơn cái app đó vì đang đem đến cho em một cô bé 
+    rất là đáng iu và dễ thương như chị hihi :> `,
+    btnIntro: 'Chị yêu em hong :> (Bấm vào đây nè)',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    desc: 'Chị có đồng ý làm người yêu bé hong <3 ? ',
+    btnYes: 'Đồng ý <3 ',
+    btnNo: 'Không nheee :>',
+    question: 'Chị có đồng ý cùng em trải qua khó khăn để xây nên 1 mái ấm hong hihi :> ?',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Có chớ sao hong hihi :))) <3 ',
+    mess: 'Em biết câu trả lời mà hihi 🥰. Em yêu chị lắm :>😘😘',
+    messDesc: 'Thế giờ em với chị làm người yêu nhauuuuu nhe :>',
+    btnAccept: 'Okiiiii bé iu <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
